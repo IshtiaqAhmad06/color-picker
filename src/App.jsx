8557a33ee3d0b583067e69ba669699047d9cdde2
Ishtiaqahmad06 <iaishtiaqahmad06@gmail.com>
2026-06-12 06:23:38 +0500

    digital Watch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 
 const App = () => {
+  const [time,setTime]=useState(new Date());
+  useEffect(()=>{
+const timer=setInterval(()=>{
+  setTime(new Date());
+},1000)
+return()=>clearInterval(timer);
+  },[])
   return (
-    <div>
-      <h1>color Picker</h1>
-      <h2> color Name is Red</h2>
-      <h3>this good name </h3>
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <div style={{width:"300px",height:"300px", display:"flex", alignContent:"center",
+    justifyContent:"center",alignItems:"center",flexDirection:"column",border:"2px solid orange"}}>
+      <h1 >Digital Watch </h1>
+      <p style={{padding:"20px",fontSize:"32px"}}>{time.toLocaleTimeString()}</p>
+      </div>
     </div>
   )
 }
